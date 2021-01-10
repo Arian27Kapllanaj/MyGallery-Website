e0e4d0c817114edc697dd49dc72c608f92b4d456
rian27Kapllanaj <arian27kapllanaj@gmail.com>
2021-01-10 21:26:10 +0100

    refactoring the code, adding header.php and forgot.php

--- login.php
+++ login.php
@@ -1,18 +1,14 @@
 <?php
+    require('header.php');
     require('authentication.php');
 ?>
 
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 <link rel="stylesheet" href="login_style.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
 
-    <header>
-        <h1>ztorex</h1>
-        <div class="line"></div>
-    </header>
     <br>
     <div class="login-form">
     <form method="post" action="login.php">
@@ -42,7 +38,7 @@
             <button type="submit" class="btn btn-primary btn-block" name="login_btn">Log in</button>
         </div>
         <div class="bottom-action clearfix">
-            <a href="#" class="float-right">Forgot your username <br>or password?</a>
+            <a href="forgot.php" class="float-right">Forgot your username <br>or password?</a>
         </div>        
     </form>
 </div>
