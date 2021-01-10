65647b453cd0ad0874b368dcaae817eeec3d54a8
rian27Kapllanaj <arian27kapllanaj@gmail.com>
2021-01-10 18:05:47 +0100

    creating the header with bootstrap in login.php and adding login_style.css

---
+++ login.php
@@ -0,0 +1,7 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+<link rel="stylesheet" href="login_style.css">
+
+<header>
+    <h1>ztorex</h1>
+    <div class="line"></div>
+</header>
