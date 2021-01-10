f3d2e7eab38028a7eaccab19423454a8f27f0f37
rian27Kapllanaj <arian27kapllanaj@gmail.com>
2021-01-10 19:06:02 +0100

    adding login form and design

--- login.php
+++ login.php
@@ -1,7 +1,44 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 <link rel="stylesheet" href="login_style.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
 
-<header>
-    <h1>ztorex</h1>
-    <div class="line"></div>
-</header>
+    <header>
+        <h1>ztorex</h1>
+        <div class="line"></div>
+    </header>
+    <br>
+    <div class="login-form">
+    <form action="#" method="post">
+        <h2>Login Form</h2>   
+        <div class="form-group">
+        	<div class="input-group">
+                <div class="input-group-prepend">
+                    <span class="input-group-text">
+                        <span class="fa fa-user"></span>
+                    </span>                    
+                </div>
+                <input type="text" class="form-control" placeholder="Username" required="required">
+            </div>
+        </div>
+		<div class="form-group">
+            <div class="input-group">
+                <div class="input-group-prepend">
+                    <span class="input-group-text">
+                        <i class="fa fa-lock"></i>
+                    </span>                    
+                </div>
+                <input type="password" class="form-control" placeholder="Password" required="required">
+            </div>
+        </div>     
+        <label class="float-left form-check-label"><input type="checkbox"> Remember me</label>   
+        <div class="form-group">
+            <button type="submit" class="btn btn-primary btn-block">Log in</button>
+        </div>
+        <div class="bottom-action clearfix">
+            <a href="#" class="float-right">Forgot your username or password?</a>
+        </div>        
+    </form>
+</div>
